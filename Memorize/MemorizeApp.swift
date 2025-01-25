48ea417d5cd36de04de250a1b7c2b686c21d9bf2
Mars-Sea <1195751600@qq.com>
2025-01-25 14:53:33 +0800

    init project

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mars_Sea on 2025/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
