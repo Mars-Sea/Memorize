96d15b040f01ad188882649c4a8a3cfcc437db84
Mars-Sea <1195751600@qq.com>
2025-01-25 15:47:27 +0800

    在view中使用ForEach循环添加视图

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis: Array<String> = ["😀", "😅", "😍", "🥳"]
+    
     var body: some View {
         HStack {
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
+            ForEach(0..<4, id: \.self) { index in
+                CardView(contant: emojis[index])
+            }
+            
+//            CardView(contant: "😀")
+//            CardView(contant: "😅")
+//            CardView(contant: "😍")
+//            CardView(contant: "🥳")
         }
         .padding()
         .foregroundStyle(.orange)
@@ -22,6 +28,7 @@ struct ContentView: View {
 
 struct CardView: View {
     @State var isFaceUp: Bool = false
+    let contant: String
     
     var body:some View {
         let base = RoundedRectangle(cornerRadius: 12)
@@ -29,7 +36,7 @@ struct CardView: View {
             if isFaceUp {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text("😀").font(.largeTitle)
+                Text(contant).font(.largeTitle)
             } else{
                 base
             }
